c7cdb15668210d37373cdf661bc8c7013e70374a
avisekk <avisekksarma@gmail.com>
2023-02-24 13:59:28 +0545

    changed database settings

--- summitseeker/summitseeker/settings.py
+++ summitseeker/summitseeker/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR/'.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv('SECRET_KEY')
-# SECRET_KEY = 'django-insecure-zu02bmud312lmroq=1$&!g3-+3=r!hce4*o(m#9zevr($e!xr1'
+# SECRET_KEY = ''
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -88,10 +88,21 @@ WSGI_APPLICATION = 'summitseeker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
